f34325c9c6391cc728b866180855639d1b38934c
Zhiyao Li <l709319582@gmail.com>
2020-10-24 12:09:40 +1100

    version 1.0 -zhiyao

---
+++ trees.js
@@ -0,0 +1,6 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoidG9yYXljYWFhIiwiYSI6ImNrZXhmOTk4YzBqb2Mydm1mZzB3cnUxNWQifQ.tCTNSJ5vcc_-pF57gh7PVw';
+        var map = new mapboxgl.Map({
+            container: 'map',
+            style: 'mapbox://styles/toraycaaa/ckglzfuyl0hba19pl7zzgf6h2', // stylesheet location
+            
+        });
