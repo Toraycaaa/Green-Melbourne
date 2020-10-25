efa98937d7da81589482d92d6606f56f6c2f4251
Zhiyao Li <l709319582@gmail.com>
2020-10-25 11:06:42 +1100

    legend - zhiyao

--- trees.js
+++ trees.js
@@ -17,9 +17,26 @@ var divElement = document.getElementById('viz1603460908310');
 
 
 mapboxgl.accessToken = 'pk.eyJ1IjoidG9yYXljYWFhIiwiYSI6ImNrZXhmOTk4YzBqb2Mydm1mZzB3cnUxNWQifQ.tCTNSJ5vcc_-pF57gh7PVw';
-       
         var map = new mapboxgl.Map({
             container: 'map',
             style: 'mapbox://styles/toraycaaa/ckglzfuyl0hba19pl7zzgf6h2', // stylesheet location
             
         });
+//zoom button
+map.addControl(new mapboxgl.NavigationControl());
+
+//hover function
+map.on('load', function() {
+    map.on('mousemove', function(e) {
+        let buildinginfo = map.queryRenderedFeatures(e.point, {
+            layers: ['New']  
+          });
+    
+        if (buildinginfo.length > 0) {
+        document.getElementById('info').innerHTML = '<p>' + buildinginfo[0].properties.status + 
+            '<p><em>' + buildinginfo[0].properties.address + '</em></p>';
+        } else {
+        document.getElementById('info').innerHTML = '<p>Hover over a shaded building for details.</p>';
+        }
+      });
+  });
