26c9e14ace8d597daa8f40e6ae1f3069b7ee72ab
eva <eva.wkx@gmail.com>
2020-10-29 02:08:43 +0800

    linked all map layers

--- trees.js
+++ trees.js
@@ -1,13 +1,4 @@
-var divElement = document.getElementById('viz1603636061401');                    
-    var vizElement = divElement.getElementsByTagName('object')[0];                    
-    if ( divElement.offsetWidth > 800 ) 
-        { vizElement.style.width='1000px';vizElement.style.height='1327px';} 
-    else if ( divElement.offsetWidth > 500 ) 
-        { vizElement.style.width='1000px';vizElement.style.height='1327px';} 
-    else { vizElement.style.width='100%';vizElement.style.height='727px';}                     
-    var scriptElement = document.createElement('script');                    
-    scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';                    
-    vizElement.parentNode.insertBefore(scriptElement, vizElement); 
+var divElement = document.getElementById('viz1603907031602');                    var vizElement = divElement.getElementsByTagName('object')[0];                    if ( divElement.offsetWidth > 800 ) { vizElement.style.width='1100px';vizElement.style.height='1527px';} else if ( divElement.offsetWidth > 500 ) { vizElement.style.width='1100px';vizElement.style.height='1527px';} else { vizElement.style.width='100%';vizElement.style.height='727px';}                     var scriptElement = document.createElement('script');                    scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';                    vizElement.parentNode.insertBefore(scriptElement, vizElement); 
 
 
 
@@ -24,96 +15,168 @@ map.addControl(new mapboxgl.NavigationControl());
 map.on('load', function() {
             
     // add source and layer for museums
-    map.addSource('New', {
+    map.addSource('Trees', {
             type: 'vector',
             url: 'mapbox://evaaaaazzz.ad4ib1uc'
     });
     map.addLayer({
         'id': 'New',
         'type': 'Symbol',
-        'source': 'New',
+        'source': 'Trees',
         'layout': {
             // make layer visible by default
             'visibility': 'none'
         },
-        'paint': {
-            'circle-radius': 8,
-            'circle-color': 'rgba(55,148,179,1)'
-        },
-        'source-layer': 'New'
+
     });
-    map.addSource('Juvenile', {
-        type: 'vector',
-        url: 'mapbox://evaaaaazzz.ad4ib1uc'
-    });
+
     map.addLayer({
         'id': 'Juvenile',
         'type': 'Symbol',
-        'source': 'Juvenile',
+        'source': 'Trees',
         'layout': {
                 // make layer visible by default
                 'visibility': 'none'
         },
-        'paint': {
-                'circle-radius': 8,
-                'circle-color': 'rgba(55,148,179,1)'
-        },
-        'source-layer': 'Juvenile'
+
     });
-    map.addSource('Semi-mature', {
-        type: 'vector',
-        url: 'mapbox://evaaaaazzz.ad4ib1uc'
-    });
+
     map.addLayer({
         'id': 'Semi-mature',
         'type': 'Symbol',
-        'source': 'Semi-mature',
+        'source': 'Trees',
         'layout': {
                 // make layer visible by default
                 'visibility': 'none'
         },
-        'paint': {
-                'circle-radius': 8,
-                'circle-color': 'rgba(55,148,179,1)'
-        },
-        'source-layer': 'Semi-mature'
+
     });
-    map.addSource('Mature', {
-        type: 'vector',
-        url: 'mapbox://evaaaaazzz.ad4ib1uc'
-    });
+
     map.addLayer({
         'id': 'Mature',
         'type': 'Symbol',
-        'source': 'Mature',
+        'source': 'Trees',
         'layout': {
                 // make layer visible by default
                 'visibility': 'none'
         },
-        'paint': {
-                'circle-radius': 8,
-                'circle-color': 'rgba(55,148,179,1)'
-        },
-        'source-layer': 'Mature'
-    });
-    map.addSource('Over-mature', {
-        type: 'vector',
-        url: 'mapbox://evaaaaazzz.ad4ib1uc'
+       
     });
     map.addLayer({
         'id': 'Over-mature',
         'type': 'Symbol',
-        'source': 'Over-mature',
+        'source': 'Trees',
         'layout': {
                 // make layer visible by default
                 'visibility': 'none'
         },
-        'paint': {
-                'circle-radius': 8,
-                'circle-color': 'rgba(55,148,179,1)'
-        },
-        'source-layer': 'Over-mature'
+
     });
+    map.addLayer({
+        'id': 'Location-Park',
+        'type': 'Symbol',
+        'source': 'Trees',
+        'layout': {
+                // make layer visible by default
+                'visibility': 'none'
+        },
+
+    });
+    map.addLayer({
+        'id': 'Location-Street',
+        'type': 'Symbol',
+        'source': 'Trees',
+        'layout': {
+            // make layer visible by default
+            'visibility': 'none'
+        },
+    });
+        map.addLayer({
+        'id': '2010-2020',
+        'type': 'Symbol',
+        'source': 'Trees',
+        'layout': {
+            // make layer visible by default
+            'visibility': 'none'
+        },
+    });
+
+    map.addLayer({
+        'id': '2000-2010',
+        'type': 'Symbol',
+        'source': 'Trees',
+        'layout': {
+            // make layer visible by default
+            'visibility': 'none'
+        },
+    });
+
+    map.addLayer({
+        'id': 'Before 2000',
+        'type': 'Symbol',
+        'source': 'Trees',
+        'layout': {
+            // make layer visible by default
+            'visibility': 'none'
+        },
+    });
+    map.addLayer({
+        'id': 'Duplex-Clay Silt then Clay',
+        'type': 'Symbol',
+        'source': 'Soil',
+        'layout': {
+            // make layer visible by default
+            'visibility': 'none'
+        },
+    });
+    map.addLayer({
+        'id': 'Medium to Heavy Textured Clay with Sand',
+        'type': 'Symbol',
+        'source': 'Soil',
+        'layout': {
+            // make layer visible by default
+            'visibility': 'none'
+        },
+    });
+    map.addLayer({
+        'id': 'Deep Stratified Sand Silt Clay Gravel',
+        'type': 'Symbol',
+        'source': 'Soil',
+        'layout': {
+            // make layer visible by default
+            'visibility': 'none'
+        },
+    });
+    map.addLayer({
+        'id': 'Silt',
+        'type': 'Symbol',
+        'source': 'Soil',
+        'layout': {
+            // make layer visible by default
+            'visibility': 'none'
+        },
+    });
+    map.addLayer({
+        'id': 'Shallow Heavy Textured Clay',
+        'type': 'Symbol',
+        'source': 'Soil',
+        'layout': {
+            // make layer visible by default
+            'visibility': 'none'
+        },
+    });
+    map.addLayer({
+        'id': 'Sandy Loam',
+        'type': 'Symbol',
+        'source': 'Soil',
+        'layout': {
+            // make layer visible by default
+            'visibility': 'none'
+        },
+    });
+
+
+
 
     // Create a popup, but don't add it to the map yet.
     var popup = new mapboxgl.Popup({
@@ -149,47 +212,157 @@ map.on('load', function() {
             popup.remove();
         });
     
-    }    
-    
+    }
 });
 
 
 // enumerate ids of the layers
-var toggleableLayerIds = ['New','Juvenile','Semi-mature','Mature','Over-mature',];
-
+var age_description = ['New','Juvenile','Semi-mature','Mature','Over-mature',];
 // set up the corresponding toggle button for each layer
+for (var i = 0; i < age_description.length; i++) {
+    var id = age_description[i];
+
+    var link = document.createElement('a');
+    // link.classList.add("active");
+    link.classList.add("item","active_map");
+    link.textContent = id;
+    link.dataValue = id
+
+    link.onclick = function (e) {
+        var clickedLayer = this.textContent;
+        e.preventDefault();
+        e.stopPropagation();
+
+        var visibility = map.getLayoutProperty(clickedLayer, 'visibility');
+        console.log('可见吗？' + visibility)
+
+        // toggle layer visibility by changing the layout object's visibility property
+        if (visibility === 'visible') {
+                map.setLayoutProperty(clickedLayer, 'visibility', 'none');
+                this.setAttribute("class","item");
+                console.log('bukejian')
+            } else {
+                this.setAttribute("class","active_map");
+                this.classList.add("item")
+                map.setLayoutProperty(clickedLayer, 'visibility', 'visible');
+                console.log('kejian')
+            }
+    };
+
+    var layers = document.getElementById('age');
+    layers.appendChild(link);
+}
 
 
-for (var i = 0; i < toggleableLayerIds.length; i++) {
-        var id = toggleableLayerIds[i];
+// enumerate ids of the layers
+var locate = ['Location-Street','Location-Park'];
+// set up the corresponding toggle button for each layer
+for (var i = 0; i < locate.length; i++) {
+    var id = locate[i];
 
-        var link = document.createElement('a');
-        // link.classList.add("active");
-        link.classList.add("item","active_map");
-        link.textContent = id;
-        link.dataValue = id
+    var link = document.createElement('b');
+    // link.classList.add("active");
+    link.classList.add("item","active_map");
+    link.textContent = id;
+    link.dataValue = id
 
-        link.onclick = function (e) {
-            var clickedLayer = this.textContent;
-            e.preventDefault();
-            e.stopPropagation();
+    link.onclick = function (e) {
+        var clickedLayer = this.textContent;
+        e.preventDefault();
+        e.stopPropagation();
 
-            var visibility = map.getLayoutProperty(clickedLayer, 'visibility');
-            console.log('可见吗？' + visibility)
+        var visibility = map.getLayoutProperty(clickedLayer, 'visibility');
+        console.log('可见吗？' + visibility)
 
-            // toggle layer visibility by changing the layout object's visibility property
-            if (visibility === 'visible') {
-                    map.setLayoutProperty(clickedLayer, 'visibility', 'none');
-                    this.setAttribute("class","item");
-                    console.log('bukejian')
-                } else {
-                    this.setAttribute("class","active_map");
-                    this.classList.add("item")
-                    map.setLayoutProperty(clickedLayer, 'visibility', 'visible');
-                    console.log('kejian')
-                }
-        };
+        // toggle layer visibility by changing the layout object's visibility property
+        if (visibility === 'visible') {
+                map.setLayoutProperty(clickedLayer, 'visibility', 'none');
+                this.setAttribute("class","item");
+                console.log('bukejian')
+            } else {
+                this.setAttribute("class","active_map");
+                this.classList.add("item")
+                map.setLayoutProperty(clickedLayer, 'visibility', 'visible');
+                console.log('kejian')
+            }
+    };
 
-        var layers = document.getElementById('age');
-        layers.appendChild(link);
-    }
+    var layers = document.getElementById('location');
+    layers.appendChild(link);
+}
+
+// enumerate ids of the layers
+var soil = ['Duplex-Clay Silt then Clay', 'Medium to Heavy Textured Clay with Sand', 'Deep Stratified Sand Silt Clay Gravel',
+            'Silt', 'Shallow Heavy Textured Clay','Sandy Loam'];
+// set up the corresponding toggle button for each layer
+for (var i = 0; i < soil.length; i++) {
+    var id = soil[i];
+
+    var link = document.createElement('c');
+    // link.classList.add("active");
+    link.classList.add("item","active_map");
+    link.textContent = id;
+    link.dataValue = id
+
+    link.onclick = function (e) {
+        var clickedLayer = this.textContent;
+        e.preventDefault();
+        e.stopPropagation();
+
+        var visibility = map.getLayoutProperty(clickedLayer, 'visibility');
+        console.log('可见吗？' + visibility)
+
+        // toggle layer visibility by changing the layout object's visibility property
+        if (visibility === 'visible') {
+                map.setLayoutProperty(clickedLayer, 'visibility', 'none');
+                this.setAttribute("class","item");
+                console.log('bukejian')
+            } else {
+                this.setAttribute("class","active_map");
+                this.classList.add("item")
+                map.setLayoutProperty(clickedLayer, 'visibility', 'visible');
+                console.log('kejian')
+            }
+    };
+
+    var layers = document.getElementById('soil_type');
+    layers.appendChild(link);
+}
+
+// enumerate ids of the layers
+var year = ['Before 2020', '2000-2010', '2010-2020'];
+// set up the corresponding toggle button for each layer
+for (var i = 0; i < year.length; i++) {
+    var id = year[i];
+
+    var link = document.createElement('c');
+    // link.classList.add("active");
+    link.classList.add("item","active_map");
+    link.textContent = id;
+    link.dataValue = id
+
+    link.onclick = function (e) {
+        var clickedLayer = this.textContent;
+        e.preventDefault();
+        e.stopPropagation();
+
+        var visibility = map.getLayoutProperty(clickedLayer, 'visibility');
+        console.log('可见吗？' + visibility)
+
+        // toggle layer visibility by changing the layout object's visibility property
+        if (visibility === 'visible') {
+                map.setLayoutProperty(clickedLayer, 'visibility', 'none');
+                this.setAttribute("class","item");
+                console.log('bukejian')
+            } else {
+                this.setAttribute("class","active_map");
+                this.classList.add("item")
+                map.setLayoutProperty(clickedLayer, 'visibility', 'visible');
+                console.log('kejian')
+            }
+    };
+
+    var layers = document.getElementById('year');
+    layers.appendChild(link);
+}
+
